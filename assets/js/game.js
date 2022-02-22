5ca6790f4cb79b49016ae7154ef7448cc2d186bd
JonathanKim424 <vizard424@yahoo.com>
2022-02-22 15:28:20 -0700

    randomized fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,44 +9,96 @@ var getPlayerName = function() {
 };
 
 var fight = function(enemy) {
-    // repeat and execute as long as the enemy-robot is alive
     while(enemy.health > 0 && playerInfo.health > 0) {
-        // ask player if they'd like to fight or skip using fightOrSkip function
         if (fightOrSkip()) {
-            // if true, leave fight by breaking loop
             break;
         }
+        
+        var isPlayerTurn = true;
 
-        // player attacks enemy
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-        enemy.health = Math.max(0, enemy.health - damage);
-        console.log (
-            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-        )
-        // check enemy health
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + " has died!");
-            break;
+        if (Math.random() > 0.5) {
+            isPlayerTurn = false;
         }
-        else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
-        }
+        
+        if (isPlayerTurn) {
+            playerAttack(enemy);
+            // check enemy health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
 
-        // enemy attack player
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-        console.log(
-            enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-        )
-        // check player health
-        if (playerInfo.health <= 0) {
-            window.alert(playerInfo.name + " has died!");
-            break;
+                // award player money for winning
+                playerInfo.money = playerInfo.money + 20;
+                break;
+            }
+            else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+            enemyAttack(enemy);
+            // check player health
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died!");
+                break;
+            }
+            else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
         }
-        else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+        if (!isPlayerTurn) {
+            enemyAttack(enemy);
+            // check player health
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died!");
+                break;
+            }
+            else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
+            playerAttack(enemy);
+            // check enemy health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
+
+                // award player money for winning
+                playerInfo.money = playerInfo.money + 20;
+                break;
+            }
+            else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
         }
     }
+};
+
+var playerAttack =  function(enemy) {
+    // player attacks enemy
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+    enemy.health = Math.max(0, enemy.health - damage);
+    console.log(
+        playerInfo.name +
+            " attacked " +
+            enemy.name +
+            ". " +
+            enemy.name +
+            " now has " +
+            enemy.health +
+            " health remaining."
+    );
+};
+
+var enemyAttack = function(enemy) {
+    // enemy attacks player
+    var damage = randomNumber(enemy.attack - 3, enemy.attack);
+    playerInfo.health = Math.max(0, playerInfo.health - damage);
+    console.log(
+        enemy.name +
+            " attacked " +
+            playerInfo.name +
+            ". " +
+            playerInfo.name +
+            " now has " +
+            playerInfo.health +
+            " health remaining."
+    );
 };
 
 // function to start a new game
